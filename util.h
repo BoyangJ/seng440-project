973401a44132a53f61cbd0f55353de2cc4c24469
BoyangJ <boyang.jiao@gmail.com>
2018-07-20 11:34:44 -0700

    initial commit

---
+++ util.h
@@ -0,0 +1,23 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+// Globals
+int height;
+int width;
+
+typedef struct rgb
+{
+    float r;
+    float g;
+    float b;
+} rgb;
+
+typedef struct ycc
+{
+    float y;
+    float cb;
+    float cr;
+} ycc;
+
+
+#endif
